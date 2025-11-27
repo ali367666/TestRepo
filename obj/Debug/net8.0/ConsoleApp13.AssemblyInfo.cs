405347006c2748757bf0a22c0fe4077679dad246
Ali <ali.axmedov.367@gmail.com>
2025-11-27 22:03:39 +0400

    Second changed

--- obj/Debug/net8.0/ConsoleApp13.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp13.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deee2b9163b6820c25378440640bda0a343f86d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp13")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
