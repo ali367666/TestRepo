174deefed1c875aa44b30bc97ada357cbb57a2dd
Ali <ali.axmedov.367@gmail.com>
2025-11-27 22:32:52 +0400

    Tgh=

--- Manager.cs
+++ Manager.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ConsoleApp13;
 
-namespace ConsoleApp13
+public class Manager:Person
 {
-    class Manager
+    public override void GetInfo()
     {
+        Console.WriteLine($"Manager name:{Name},Manager surname:{Surname}");
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,3 +4,8 @@ Developer developer = new Developer();
 developer.Name = "Ali";
 developer.Surname = "Ahmadov";
 developer.GetInfo();
+
+Manager manager = new Manager();
+manager.Name = "Veli";
+manager.Surname = "Veliyev";
+manager.GetInfo();
